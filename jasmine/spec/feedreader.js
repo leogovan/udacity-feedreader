7435fb3bf6bb52307c3a76d18b8270e5d8b6eabe
Leofric Govan <leogovan@Leos-MacBook-Pro-2.local>
2017-06-06 22:44:28 +0100

    Add test for menu element hidden by default

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -56,12 +56,16 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
-    describe("", function() {
+    describe("The menu", function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+         it("menu element is hidden by default", function(){
+            var a = document.getElementsByClassName("menu-hidden");
+            expect(a).toBeDefined();
+         });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
@@ -71,19 +75,21 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
-    describe("", function() {
+    describe("Initial Entries", function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
-
+    describe("Initial Entries", function() {
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
     });
+        
 }());
